c905f7242cd53e8cecb37134538342e4d3932089
Eliakah <9116161+eliakah@users.noreply.github.com>
2024-10-10 19:16:46 -0400

    Added React files
    Added React files for front end

---
+++ src/frontend/react-app/src/Message.tsx
@@ -0,0 +1,10 @@
+// always use PascaleCasing
+function Message() {
+    //JSX: Javascript XML
+    const name = 'Eliakah'; 
+    if (name)
+        return <h1> Hello {name}</h1>; 
+    return <h1>Hello world</h1>;
+}
+
+export default Message;
